6b9b4f50ad9be7cf6a8da0f3eaef7fe08698fa78
Wilson Borba <asasdeaguia10@gmail.com>
2024-12-02 10:19:10 +0700

    feat(reproduce-route): add reproduce route to download and serve audio files

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, Response, stream_with_context
+import os
+from flask import Flask, Response, send_file, stream_with_context
 import subprocess
 
 app = Flask(__name__)
@@ -6,16 +7,20 @@ app = Flask(__name__)
 @app.route('/stream/<video_id>')
 def stream_audio(video_id):
     def generate_audio():
+
+
         command = [
             'yt-dlp',
-            '-o', '-',
-            '-f', 'bestaudio[ext=m4a]',  # Garante compatibilidade com Safari
+            '-o', '-',          # Saída em stdout
+            '-f', 'bestaudio',  # Melhor formato de áudio disponível
             f'https://www.youtube.com/watch?v={video_id}'
         ]
-        process = subprocess.Popen(command, stdout=subprocess.PIPE)
+
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         try:
+
             while True:
-                chunk = process.stdout.read(4096)
+                chunk = process.stdout.read(8192)
                 if not chunk:
                     break
                 yield chunk
@@ -23,12 +28,28 @@ def stream_audio(video_id):
             process.kill()
 
     headers = {
-        "Content-Type": "audio/mp4",
-        "Cache-Control": "no-cache",
-        "Connection": "keep-alive",
-    }
-    return Response(stream_with_context(generate_audio()), headers=headers, mimetype='audio/mp4')
+        "Cache-Control": "no-cache",      # Prevent caching for real-time streaming
+        "Connection": "keep-alive",       # Keeps the connection open for streaming
+        }
+    
+    return Response(stream_with_context(generate_audio()),headers=headers ,mimetype='audio/mpeg')
+
+
+@app.route('/reproduce/<video_id>')
+def reproduce_audio(video_id):
+    audio_file = f'{video_id}.m4a'
+    if not os.path.exists(audio_file):
+        # Baixa o áudio se não existir
+        command = [
+            'yt-dlp',
+            '-o', audio_file,
+            '-f', 'bestaudio[ext=m4a]',
+            f'https://www.youtube.com/watch?v={video_id}'
+        ]
+        subprocess.run(command)
+
+    # Serve o arquivo de áudio baixado
+    return send_file(audio_file, mimetype='audio/mp4', as_attachment=False)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
